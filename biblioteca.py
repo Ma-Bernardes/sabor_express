a576a30bcee1ceeaf07c23169313b7632e988503
Marcelo Bernardes <138727625+Ma-Bernardes@users.noreply.github.com>
2024-04-27 08:08:30 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,8 @@
+from tarefa import Livro
+
+
+
+
+ano_especifico = 2023
+livros_disponiveis_ano = Livro.verificar_disponibilidade(ano_especifico)
+print(f"Livros disponíveis em {ano_especifico}: {livros_disponiveis_ano}")
